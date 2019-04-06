33c79201adc0c09e5b1517721632784bfe71ddc7
javiersm <javier.santana.mejias@gmail.com>
2019-04-06 11:04:07 +0200

    aplication with routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JS
+ * Date: 06/04/2019
+ * Time: 9:58
+ */
+
+import {RouterModule, Routes} from '@angular/router';
+
+
+
